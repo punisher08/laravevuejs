d2d3ae91b42324dda0d631479284374ad6c56f1e
jhomel <jhomignacio08@gmail.com>
2021-06-05 13:31:57 +0800

    created first component API with axios

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="first-component">
+  <h1>First Component</h1>
+  <ul>
+    <li v-for="post in posts">{{post.title}}</li>
+  </ul>
+  
+  </div>
+</template>
+<script>
+ export default {
+   mounted: function(){
+        axios.get('https://jsonplaceholder.typicode.com/todos').then(response => this.posts = response.data)
+        .catch(
+          this.posts = [{title:'No Records found'}]
+          ).finally(() => console.log('data loading'))
+    },
+    data: function(){
+      return{
+        posts:[]
+      }
+    }
+
+    }
+</script>
+<style></style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<home-component></home-component>
+
+
+</div>
+@endsection
